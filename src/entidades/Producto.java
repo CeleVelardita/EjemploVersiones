9cf1bb004e19fa79dbcdbb5ca3b735d6112ddd41
orli1208 <116685619+orli1208@users.noreply.github.com>
2023-08-07 19:45:02 -0300

    Agregue clase producto

---
+++ src/entidades/Producto.java
@@ -0,0 +1,23 @@
+
+package entidades;
+
+public class Producto {
+    private int codigo;
+
+    public Producto(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    
+    
+    
+    
+}
